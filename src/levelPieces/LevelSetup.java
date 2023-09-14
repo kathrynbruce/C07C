efa22236ddcb1b9b1f3176bc43f4306be65cd011
rhcastro <94096212+rocky-mtn-high@users.noreply.github.com>
2023-09-14 11:26:19 -0600

    Added LevelSetup Shells

---
+++ src/levelPieces/LevelSetup.java
@@ -0,0 +1,29 @@
+package levelPieces;
+
+import java.util.ArrayList;
+import gameEngine.Drawable;
+import gameEngine.Moveable;
+import gameEngine.Player;
+
+public class LevelSetup {
+
+    public void createLevel(int levelNum) {
+        return;
+    }
+
+    public Drawable[] getBoard() {
+        return null;
+    }
+
+    public ArrayList<Moveable> getMovingPieces() {
+        return null;
+    }
+
+    public ArrayList<GamePiece> getInteractingPieces() {
+        return null;
+    }
+
+    public void getPlayerStartLocation(Player player) {
+        return;
+    }
+}
