84b6ab574ef7bf08d9a9461240e0461ccad3ef41
rhcastro <94096212+rocky-mtn-high@users.noreply.github.com>
2023-09-18 20:13:36 -0600

    Level functionality!

--- src/levelPieces/LevelSetup.java
+++ src/levelPieces/LevelSetup.java
@@ -5,21 +5,53 @@ import gameEngine.Drawable;
 import gameEngine.Moveable;
 
 public class LevelSetup {
+    ArrayList<Moveable> movingPieces;
+    ArrayList<GamePiece> interactingPieces;
+    Drawable[] gameBoard;
 
     public void createLevel(int levelNum) {
-        return;
+        movingPieces = new ArrayList<Moveable>();
+        interactingPieces = new ArrayList<GamePiece>();
+        // setup game board
+        gameBoard = new Drawable[20];
+
+        // level 1: easy peasy
+        if (levelNum == 1) {
+
+            // add drawable piece
+            gameBoard[5] = new Token(5);
+
+            // add interacting pieces
+            gameBoard[0] = new Unicorn(0);
+            gameBoard[7] = new Ghost(7);
+            gameBoard[9] = new Ghost(9);
+            gameBoard[12] = new Cavalry(12);
+
+            // put them in arrays
+            interactingPieces.add((GamePiece) gameBoard[0]);
+            interactingPieces.add((GamePiece) gameBoard[7]);
+            interactingPieces.add((GamePiece) gameBoard[9]);
+            interactingPieces.add((GamePiece) gameBoard[12]);
+            movingPieces.add((Moveable) gameBoard[0]);
+            movingPieces.add((Moveable) gameBoard[7]);
+            movingPieces.add((Moveable) gameBoard[9]);
+            movingPieces.add((Moveable) gameBoard[12]);
+
+        }
+
+        // level 2: death
     }
 
     public Drawable[] getBoard() {
-        return null;
+        return gameBoard;
     }
 
     public ArrayList<Moveable> getMovingPieces() {
-        return null;
+        return movingPieces;
     }
 
     public ArrayList<GamePiece> getInteractingPieces() {
-        return null;
+        return interactingPieces;
     }
 
     public int getPlayerStartLoc() {
